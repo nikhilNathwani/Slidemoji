0302dfd4a8fee9de5b3a315317f87a9037a3b60a
nikhilNathwani <nnathwani36@gmail.com>
2026-03-02 18:25:41 -0500

    delay-loading font awesome. improves initial page load performance since Font Awesome (~100KB+) won't be loaded until users actually start playing. The landing page's play button now uses a lightweight inline SVG that looks identical to the Font Awesome icon.

--- src/components/landing/LandingPage.jsx
+++ src/components/landing/LandingPage.jsx
@@ -1,7 +1,14 @@
 import AnimatedTileGrid from "./AnimatedTileGrid";
 import LandingFooter from "./LandingFooter";
+import { loadFontAwesome } from "../../utils/fontAwesome";
 
 function LandingPage({ onPlay }) {
+	const handlePlay = () => {
+		// Load Font Awesome before transitioning to game
+		loadFontAwesome();
+		onPlay();
+	};
+
 	return (
 		<div className="landing-page">
 			<div className="landing-content">
@@ -11,8 +18,17 @@ function LandingPage({ onPlay }) {
 					Slide the tiles to unscramble the daily emoji
 				</p>
 
-				<button className="play-button" onClick={onPlay}>
-					<i className="fas fa-play"></i>
+				<button className="play-button" onClick={handlePlay}>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 384 512"
+						fill="currentColor"
+						style={{
+							height: "1.1rem",
+						}}
+					>
+						<path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z" />
+					</svg>
 					Play
 				</button>
 			</div>
