b5a26b57569730d7a42dd6c5b685f0910025d6bd
nikhilNathwani <nnathwani36@gmail.com>
2026-04-22 18:11:57 -0400

    style: reformat StatsContent archive section JSX

--- src/components/stats/StatsContent.jsx
+++ src/components/stats/StatsContent.jsx
@@ -41,38 +41,42 @@ function StatsContent({
 
 					{/* Archive section */}
 					{/* <div className={styles.statsDivider}></div> */}
-					{showArchiveSection && (isPremium ? (
-						showPremiumArchiveHint && (
-						<div className={styles.archiveSection}>
-							<p className={styles.archiveDescription}>
-								Use the{" "}
-								<strong>
-									Puzzle Archive{" "}
-									<FontAwesomeIcon icon={faClockRotateLeft} />
-								</strong>{" "}
-								button in the header to play past puzzles and
-								add to your trophy collection.
-							</p>
-						</div>
-						)
-					) : (
-						<div className={styles.archiveSection}>
-							<h3 className={styles.archiveTitle}>
-								Puzzle Archive
-							</h3>
-							<p className={styles.archiveDescription}>
-								Play past puzzles to earn missed trophies and
-								complete your collection.
-							</p>
-							<button
-								className="btn btn-solid"
-								onClick={onUnlockArchiveClick}
-							>
-								<FontAwesomeIcon icon={faUnlock} />
-								Unlock Archive
-							</button>
-						</div>
-					))}
+					{showArchiveSection &&
+						(isPremium ? (
+							showPremiumArchiveHint && (
+								<div className={styles.archiveSection}>
+									<p className={styles.archiveDescription}>
+										Use the{" "}
+										<strong>
+											Puzzle Archive{" "}
+											<FontAwesomeIcon
+												icon={faClockRotateLeft}
+											/>
+										</strong>{" "}
+										button in the header to play past
+										puzzles and add to your trophy
+										collection.
+									</p>
+								</div>
+							)
+						) : (
+							<div className={styles.archiveSection}>
+								<h3 className={styles.archiveTitle}>
+									Puzzle Archive
+								</h3>
+								<p className={styles.archiveDescription}>
+									Play past puzzles to earn missed trophies
+									and complete your collection.
+								</p>
+								<button
+									className="btn btn-solid"
+									onClick={onUnlockArchiveClick}
+								>
+									<FontAwesomeIcon icon={faUnlock} />
+									Unlock Archive
+								</button>
+							</div>
+						))}
 				</>
 			)}
 		</div>
